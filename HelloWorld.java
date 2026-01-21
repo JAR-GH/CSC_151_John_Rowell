7b1d222d04ffbbb9d6d117c6e058058e29962379
SANDHILLS\LH218-12 <jad.talbert@gmail.com>
2026-01-21 11:18:17 -0500

    First program done!

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,1 +1,13 @@
+/*
+@author: John Rowell
+@date: 1/21/2026
+@purpose: write first program of the semester
+*/
 
+public class HelloWorld
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World");
+    }
+}
